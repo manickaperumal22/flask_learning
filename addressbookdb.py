499033fa0892aae117ab7023dcf50e82af93b138
manickaperumal <manickaperumal@altrosyn.com>
2022-05-05 12:02:15 +0530

    all files

---
+++ addressbookdb.py
@@ -0,0 +1,8 @@
+import sqlite3
+connection=sqlite3.Connection("addressbook.db")
+print("database opened successfully")
+cursor=connection.cursor()
+# cursor.execute('''DROP TABLE address;''')
+connection.execute("create table address(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL,email TEXT UNIQUE NOT NULL,address TEXT NOT NULL)")
+print("table created successfully")
+connection.close()
